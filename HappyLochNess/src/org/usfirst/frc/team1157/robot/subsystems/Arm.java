bba8d963edb16c7b792ae167e3b41f4267fe8dd3
Sharky <claire@carlin.com>
2016-01-30 15:43:10 -0700

    stuff ya

---
+++ HappyLochNess/src/org/usfirst/frc/team1157/robot/subsystems/Arm.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team1157.robot.subsystems;
+
+import org.usfirst.frc.team1157.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class Arm extends PIDSubsystem {
+    
+	AnalogInput pot = RobotMap.pot;
+	CANJaguar motor = RobotMap.frontRightMotor;
+	
+	public Arm(double p, double i, double d) {
+		super("Arm", p, i, d);
+		setAbsoluteTolerance(0.05); //Set the absolute error which is considered tolerable for use with OnTarget. The value is in the same range as the PIDInput values.
+		getPIDController().setContinuous(false);
+	}
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    protected double returnPIDInput() {
+    	return pot.getAverageVoltage();
+    }
+    
+    protected void usePIDOutput(double output) {
+    	motor.pidWrite(output);
+    	SmartDashboard.putNumber("pidOut", output);
+    }
+}
+
---
+++ HappyLochNess/src/org/usfirst/frc/team1157/robot/subsystems/ArmWithoutPID.java
@@ -0,0 +1,53 @@
+package org.usfirst.frc.team1157.robot.subsystems;
+
+import org.usfirst.frc.team1157.robot.RobotMap;
+import org.usfirst.frc.team1157.robot.commands.ArmWithJoystick;
+
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class ArmWithoutPID extends Subsystem {
+
+	CANJaguar motor = RobotMap.armMotor;
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	public void initDefaultCommand(){
+		
+		motor.setPercentMode();
+		motor.enableControl();
+		motor.set(0.0);
+		// Set the default command for a subsystem here.
+
+		setDefaultCommand(new ArmWithJoystick());
+	}
+
+	public void armControl(Joystick joy) {
+		motor.set(joy.getY());
+	}
+
+	public boolean speed(double speed) {
+		if (speed < 1 || speed > -1) {
+			motor.set(speed);
+		} else {
+			return false;
+		}
+		return true;
+	}
+
+	public void forward() {
+		motor.set(1.0);
+
+	}
+
+	public void backward() {
+		motor.set(-1.0);
+	}
+
+	public void stop() {
+		motor.set(0.0);
+	}
+}
+
+
