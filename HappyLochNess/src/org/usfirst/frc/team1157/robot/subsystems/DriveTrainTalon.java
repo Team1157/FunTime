f8933e8b67f11ae38a27c4bd8faaeb075fda0f64
Sharky <claire@carlin.com>
2016-02-17 17:03:58 -0700

    added testing mode to driveTrainTalon

--- HappyLochNess/src/org/usfirst/frc/team1157/robot/subsystems/DriveTrainTalon.java
+++ HappyLochNess/src/org/usfirst/frc/team1157/robot/subsystems/DriveTrainTalon.java
@@ -4,7 +4,10 @@ import org.usfirst.frc.team1157.robot.RobotMap;
 import org.usfirst.frc.team1157.robot.commands.TankDriveWithJoystick;
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.FeedbackDevice;
+import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -13,19 +16,48 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class DriveTrainTalon extends Subsystem {
-	
-	CANTalon frontRight = new CANTalon(RobotMap.frontRightMotor);
-	CANTalon frontLeft = new CANTalon(RobotMap.frontLeftMotor);
-	CANTalon backRight = new CANTalon(RobotMap.backRightMotor);
-	CANTalon backLeft = new CANTalon(RobotMap.backLeftMotor);
-	
+
+	boolean testing = false;
+
+	CANTalon right = new CANTalon(RobotMap.frontRightMotor);
+	CANTalon left = new CANTalon(RobotMap.frontLeftMotor);
+	CANTalon rightSlave = new CANTalon(RobotMap.backRightMotor);
+	CANTalon leftSlave = new CANTalon(RobotMap.backLeftMotor);
+
 	private RobotDrive drive;
 
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 	public DriveTrainTalon() {
 		super();
-		drive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
+
+		rightSlave.changeControlMode(TalonControlMode.Follower);
+		rightSlave.set(right.getDeviceID());
+
+		leftSlave.changeControlMode(TalonControlMode.Follower);
+		leftSlave.set(left.getDeviceID());
+
+		right.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+		right.reverseSensor(false);
+		right.configNominalOutputVoltage(+0.0f, -0.0f);
+		right.configPeakOutputVoltage(+12.0f, -12.0f);
+		right.setProfile(0);
+		right.setF(0);
+		right.setP(0);
+		right.setI(0);
+		right.setD(0);
+
+		left.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+		left.reverseSensor(false);
+		left.configNominalOutputVoltage(+0.0f, -0.0f);
+		left.configPeakOutputVoltage(+12.0f, -12.0f);
+		left.setProfile(0);
+		left.setF(0);
+		left.setP(0);
+		left.setI(0);
+		left.setD(0);
+
+		drive = new RobotDrive(left, right);
 
 	}
 
@@ -44,20 +76,42 @@ public class DriveTrainTalon extends Subsystem {
 	}
 
 	public void driveJoy(Joystick joy) {
-		if (joy.getName().equals("Logitech Extreme 3D")) {
-			if (joy.getTrigger()) {
-				driveArcade(joy.getY(), -joy.getZ() * 0.75);
+
+		SmartDashboard.putNumber("motorOutputRight", (right.getOutputVoltage() / right.getBusVoltage()));
+		SmartDashboard.putNumber("motorOutputLeft", (left.getOutputVoltage() / left.getBusVoltage()));
+
+		if (testing) {
+
+			if (joy.getRawButton(7)) {
+				double targetSpeed = joy.getAxis(AxisType.kY) * 1500;
+				left.changeControlMode(TalonControlMode.Speed);
+				right.changeControlMode(TalonControlMode.Speed);
+				left.set(targetSpeed);
+				right.set(targetSpeed);
 			} else {
-				driveArcade(-joy.getY(), -joy.getZ() * 0.75);
+				left.changeControlMode(TalonControlMode.PercentVbus);
+				right.changeControlMode(TalonControlMode.PercentVbus);
+				left.set(joy.getAxis(AxisType.kY));
+				right.set(joy.getAxis(AxisType.kY));
 			}
-		} else if (joy.getName().equals("Logitech RumblePad 2 USB")) {
-			if (joy.getRawButton(6)) {
-				driveTank(joy.getThrottle(), joy.getY(), true);
-			} else {
-				driveTank(-joy.getY(), -joy.getThrottle(), true);
-			}
+
 		} else {
-			SmartDashboard.putString("name", joy.getName());
+
+			if (joy.getName().equals("Logitech Extreme 3D")) {
+				if (joy.getTrigger()) {
+					driveArcade(joy.getY(), -joy.getZ() * 0.75);
+				} else {
+					driveArcade(-joy.getY(), -joy.getZ() * 0.75);
+				}
+			} else if (joy.getName().equals("Logitech RumblePad 2 USB")) {
+				if (joy.getRawButton(6)) {
+					driveTank(joy.getThrottle(), joy.getY(), true);
+				} else {
+					driveTank(-joy.getY(), -joy.getThrottle(), true);
+				}
+			} else {
+				SmartDashboard.putString("name", joy.getName());
+			}
 		}
 	}
 }
